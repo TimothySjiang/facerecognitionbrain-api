da07bfd8d30a908b33033bce1fa666f6cbca2069
TimothySjiang <shjiang@ucdavis.edu>
2019-08-13 05:48:08 -0700

    db conection

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-asymmetrical-84015',
-    user : 'postgres',
-    password : 'shan',
-    database : 'smartbrain'
+    host : process.env.DATABASE_URL,
+    ssl:true,
   }
 });
 
